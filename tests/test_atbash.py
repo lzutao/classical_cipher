658e8035e0a5a890efa43f6ad9a347ddcbc86986
Lzu Tao <taolzu@gmail.com>
2018-03-19 08:36:49 +0700

    add test for atbash

---
+++ tests/test_atbash.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python2
+
+import unittest
+
+try:
+	from classical_cipher.atbash import Atbash
+except ImportError:
+	import sys
+	import os
+	sys.path.append( os.path.dirname( os.path.dirname( os.path.abspath(__file__) ) ) )
+	from classical_cipher.atbash import Atbash
+
+
+class TestAtbash(unittest.TestCase):
+	def test_encode(self):
+		plaintext = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+		ciphertext = 'zyxwvutsrqponmlkjihgfedcbaZYXWVUTSRQPONMLKJIHGFEDCBA'
+		result = Atbash.encode(plaintext)
+		self.assertEqual(result, ciphertext)
+
+if __name__ == '__main__':
+	unittest.main()
+
